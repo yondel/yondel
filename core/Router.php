26880cd936d9f21963daf9fdb8bacd518e0e98f5
Hironori Oka <hironorioka28@gmail.com>
2014-07-12 19:10:11 +0900

    modified Router.php

--- core/Router.php
+++ core/Router.php
@@ -29,4 +29,21 @@ class Router
 
         return $routes;
     }
+
+    public function resolve($path_info)
+    {
+        if ('/' !== substr($path_info, 0, 1)) {
+            $path_info = '/' . $path_info;
+        }
+
+        foreach ($this->routes as $pattern => $params) {
+            if (preg_match('#^' . $pattern . '$#', $path_info, $matches)) {
+                $params = array_merge($params, $matches);
+
+                return $params;
+            }
+        }
+
+        return false;
+    }
 }
