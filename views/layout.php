9f5d6f59728ffc5f2c628c0e085b11d0ee2e2f64
Hironori Oka <hironorioka28@gmail.com>
2014-07-12 22:03:50 +0900

    add layout.php

---
+++ views/layout.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<title><?php if (isset($title)) : echo $this->escape($title) . ' - '; endif; ?>yondel</title>
+</head>
+<body>
+<header>
+<h1><a href="<?php echo $base_url; ?>">yondel</a></h1>
+</header>
+<section>
+<?php echo $_content; ?>
+</section>
+<footer>
+<p>&copy;<?php echo date('Y') ?> yondel, All rights reserved.</p>
+</footer>
+</body>
+</html>
