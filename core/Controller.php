282e3c0d8be900a63c9074c3458731f16d30569b
Hironori Oka <hironorioka28@gmail.com>
2014-07-12 21:24:17 +0900

    modified Controller.php

--- core/Controller.php
+++ core/Controller.php
@@ -9,6 +9,7 @@ abstract class Controller
     protected $response;
     protected $session;
     protected $db_manager;
+    protected $auth_actions = array();
 
     public function __construct($application)
     {
@@ -30,9 +31,22 @@ abstract class Controller
             $this->forward404();
         }
 
+        if ($this->needsAuthentication($action) && ! $this->session->isAuthenticated()) {
+            throw new UnauthorizedActionException();
+        }
+
         $content = $this->$action_method($params);
 
         return $content;
+    }
+
+    protected function needsAuthentication($action)
+    {
+        if ($this->auth_actions === true || (is_array($this->auth_actions) && in_array($action, $this->auth_actions))) {
+            return true;
+        }
+
+        return false;
     }
 
     protected function render($variables = array(), $template = null, $layout = 'layout')
